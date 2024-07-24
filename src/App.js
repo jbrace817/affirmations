a6670faae7146c9ab85043e06ee3e485294f9372
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-07-24 12:56:16 -0400

    initial commit
    swiper and bootstrap libraries added. no data or features added yet.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-function App() {
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import './index.css';
+
+// Import required modules
+import { Navigation } from 'swiper/modules';
+
+//Import Bootstrap styles
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Swiper
+        rewind={true}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <span className="center">Slide 1</span>
+          <img
+            src="https://picsum.photos/seed/slide1/1920/1080"
+            alt="Slide 1"
+            className="img-fluid"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <span className="center">Slide 2</span>
+          <img
+            src="https://picsum.photos/seed/slide2/1920/1080"
+            alt="Slide 2"
+            className="img-fluid"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <span className="center">Slide 3</span>
+          <img
+            src="https://picsum.photos/seed/slide3/1920/1080"
+            alt="Slide 3"
+            className="img-fluid"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <span className="center">Slide 4</span>
+          <img
+            src="https://picsum.photos/seed/slide4/1920/1080"
+            alt="Slide 4"
+            className="img-fluid"
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
-
-export default App;
