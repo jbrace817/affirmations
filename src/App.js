11c55e1265b42da22f24a5599608b3a6ba650e30
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-07-25 10:00:36 -0400

    feat: Local Storage and AutoPlay added
    LocalStorage added to start on new slide every day. Autoplay feature enabled with a delay of 5000ms.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -9,22 +9,37 @@ import 'swiper/css/navigation';
 import './index.css';
 
 // Import required modules
-import { Navigation } from 'swiper/modules';
+import { Navigation, Autoplay } from 'swiper/modules';
 
 //Import Bootstrap styles
 import 'bootstrap/dist/css/bootstrap.css';
 
+//Import Data
 import affirmations from './data';
 
 export default function App() {
+  // const today = new Date('7/25/2024').toLocaleDateString();
+  if (!localStorage.getItem('date')) {
+    localStorage.setItem('date', new Date().toLocaleDateString());
+    localStorage.setItem('slide', 0);
+  } else if (localStorage.getItem('date') !== new Date().toLocaleDateString()) {
+    localStorage.setItem('date', new Date().toLocaleDateString());
+    const savedSlide = localStorage.getItem('slide');
+    localStorage.setItem('slide', Number(savedSlide) + 1);
+    if (Number(localStorage.getItem('slide')) === affirmations.length) {
+      localStorage.setItem('slide', 0);
+    }
+  }
+
   return (
     <div className="container">
       <Swiper
         rewind={true}
         navigation={true}
-        modules={[Navigation]}
+        modules={[Navigation, Autoplay]}
         className="mySwiper"
-        initialSlide={1}
+        initialSlide={Number(localStorage.getItem('slide'))}
+        autoplay={{ delay: 5000 }}
       >
         {affirmations.map((affirmation) => (
           <SwiperSlide key={affirmation.id}>
@@ -39,9 +54,9 @@ export default function App() {
 function Affirmation({ affirmation }) {
   return (
     <>
-      <span className="center display-4 fw-normal fst-italic text-muted">
+      <h1 className="center fw-normal fst-italic text-muted ">
         {affirmation.affirmation}
-      </span>
+      </h1>
       <img
         src={`https://picsum.photos/seed/slide${affirmation.id}/1920/1080`}
         alt={`Slide ${affirmation.id}`}
